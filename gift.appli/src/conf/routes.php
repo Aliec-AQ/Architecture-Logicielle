3438f506e5cf4172c63c5c6520abe8df7bfe63b5
Aliec <amaury.quilliec@icloud.com>
2024-05-23 10:24:30 +0200

    named Routes

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -7,19 +7,19 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 return function( \Slim\App $app): \Slim\App {
 
     // route globale pour accéder rapidement aux différentes pages
-    $app->get('/', gift\appli\app\actions\GetRootAction::class);
+    $app->get('/', gift\appli\app\actions\GetRootAction::class)->setName('root');
 
-    $app->get('/categories[/]', gift\appli\app\actions\GetCategorieAction::class);
+    $app->get('/categories[/]', gift\appli\app\actions\GetCategorieAction::class)->setName('categories');
 
-    $app->get('/categorie/{id}[/]', gift\appli\app\actions\GetCategorieIdAction::class);
+    $app->get('/categorie/{id}[/]', gift\appli\app\actions\GetCategorieIdAction::class)->setName('categorieById');
 
-    $app->get('/categorie/{id}/prestations[/]', gift\appli\app\actions\GetPrestationsCategorieAction::class);
+    $app->get('/categorie/{id}/prestations[/]', gift\appli\app\actions\GetPrestationsCategorieAction::class)->setName('prestationsCategorie');
 
-    $app->get('/prestation[/]', gift\appli\app\actions\GetPrestationAction::class);
+    $app->get('/prestation[/]', gift\appli\app\actions\GetPrestationAction::class)->setName('prestations');
 
-    $app->get('/box/create[/]', gift\appli\app\actions\GetBoxCreateAction::class);
+    $app->get('/box/create[/]', gift\appli\app\actions\GetBoxCreateAction::class)->setName('boxCreate');
 
-    $app->post('/box/create[/]', gift\appli\app\actions\PostBoxCreateAction::class);
+    $app->post('/box/create[/]', gift\appli\app\actions\PostBoxCreateAction::class)->setName('boxCreatePost');
 
     return $app;
 };
