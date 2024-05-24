bedb063fc69443ff37d5b0c573a0deec7ea4e829
Aliec <amaury.quilliec@icloud.com>
2024-05-24 11:50:39 +0200

    Modification pour docker

--- gift.appli/src/conf/bootstrap.php
+++ gift.appli/src/conf/bootstrap.php
@@ -5,14 +5,10 @@ gift\appli\app\utils\Eloquent::init(__DIR__ . '/gift.db.conf.ini.dist');
 
 /* application bootstrap */
 
-// variable pour mettre le chemin de base de l'application (car dans un sous-dossier)
-$basePath = "";
-
 $app = \Slim\Factory\AppFactory::create();
 
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, false, false);
-$app->setBasePath($basePath);
 
 /* Config Twig */
 
