b8795c8c4fd388976c95340c227106996a30359d
Aliec <amaury.quilliec@icloud.com>
2024-05-28 14:39:32 +0200

    CRSF Token et css

---
+++ gift.appli/src/app/utils/CsrfService.php
@@ -0,0 +1,18 @@
+<?php
+namespace gift\appli\app\utils;
+
+class CsrfService {
+    public static function generate(){
+        $token = bin2hex(openssl_random_pseudo_bytes(32));
+        $_SESSION['giftbox_csrf_token'] = $token;
+        return $token;
+    }
+
+    public static function check($token){
+        if (!isset($_SESSION['giftbox_csrf_token']) || $_SESSION['giftbox_csrf_token'] !== $token) {
+            throw new \Exception('CSRF token verification failed');
+        }
+
+        unset($_SESSION['giftbox_csrf_token']);
+    }
+}
