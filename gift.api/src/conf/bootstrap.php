fd5ceb0e52a7fa28792c4e606ae9a7b7da33f28c
EtiqueKevin <ketique04@gmail.com>
2024-06-05 11:16:35 +0200

    handler pour l'erreur 404 de l'api (a modifier peut etre, a voir avec le prof)

--- gift.api/src/conf/bootstrap.php
+++ gift.api/src/conf/bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Psr7\Response;
+
 gift\api\infrastructure\Eloquent::init(__DIR__ . '/gift.db.conf.ini.dist');
 
 /* application bootstrap */
@@ -11,7 +15,16 @@ $app = \Slim\Factory\AppFactory::create();
 
 $app->addRoutingMiddleware();
 $app->addBodyParsingMiddleware();
-$app->addErrorMiddleware(true, false, false);
+$errorMiddleware = $app->addErrorMiddleware(true, false, false);
+
+$errorMiddleware->setErrorHandler(
+    HttpNotFoundException::class,
+    function (ServerRequestInterface $request) {
+        $response = new Response();
+        $response->getBody()->write(json_encode(['error' => 'Not found']));
+        return $response->withStatus(404);
+    }
+);
 
 $app=(require_once __DIR__ . '/routes.php')($app);
 
